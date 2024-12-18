82b8608d7720c8ffbfe72d654c3ea4aa0ee87692
Sheron <sheronroy2003@gmail.com>
2024-12-18 12:11:54 +0530

    Addition updated

--- app/src/main/java/com/example/calculatornew/AdditionActivity.java
+++ app/src/main/java/com/example/calculatornew/AdditionActivity.java
@@ -1,6 +1,10 @@
 package com.example.calculatornew;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AdditionActivity extends AppCompatActivity {
-
+    EditText et1,et2;
+    TextView t1;
+    int num1,num2,res;
+    String getNum1,getNum2,Result;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,22 @@ public class AdditionActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        et1=(EditText) findViewById(R.id.num1);
+        et2=(EditText) findViewById(R.id.num2);
+        t1=(TextView) findViewById(R.id.restxt);
+        b1=(Button) findViewById(R.id.addbt);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getNum1=et1.getText().toString();
+                getNum2=et2.getText().toString();
+                num1=Integer.parseInt(getNum1);
+                num2=Integer.parseInt(getNum2);
+                res=num1+num2;
+                Result=String.valueOf(res);
+                t1.setText(Result);
+            }
         });
     }
 }
