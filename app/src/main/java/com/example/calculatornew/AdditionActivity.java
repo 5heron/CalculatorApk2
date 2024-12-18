f736970953f5d77a4566ded07e2b1f4ca065a5e5
Sheron <sheronroy2003@gmail.com>
2024-12-18 12:00:08 +0530

    Menu updated

--- app/src/main/java/com/example/calculatornew/AdditionActivity.java
+++ app/src/main/java/com/example/calculatornew/AdditionActivity.java
@@ -1,9 +1,6 @@
 package com.example.calculatornew;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button b1;
+public class AdditionActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_addition);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        b1=(Button) findViewById(R.id.addbt);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent ob = new Intent(getApplicationContext(), AdditionActivity.class);
-                startActivity(ob);
-            }
         });
     }
 }
